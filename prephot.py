233753b5ca4bda3567e949f051151d0842e6c030
NiBr B <NiBr@MIT.edu>
2014-07-25 06:00:00 -0400

    prephot knows what it's looking at and gets sources list

--- prephot.py
+++ prephot.py
@@ -52,28 +52,59 @@ def getExoplanetList():
         exoDic.append(dic)
     return exoDic
 
-def areEqual(a, b, precision = 3):
-    return abs(a-b) <= pow(10, -precision)
+def areEqual(a, b, precision = 10**(-3)):
+    return abs(float(a)-float(b)) <= precision
 
 exoStarList = getExoplanetList()
+runs = {}
 for bfn in baseFnList:
     print("opening " + bfn)
-    f = pyfits.open(bfn + ".new")
-    starList = f[1].data
+    hdr = fs.getHeader(bfn + ".new")
+    date = hdr["DATE-OBS"].split("T")[0]
+
+    wcsList = [i.split(" ") for i in fs.readFileToArray(bfn + ".wcs.center")]
+    wcsDict = {}
+    for key, value in wcsList:
+        wcsDict[key] = value
+
     ooi = {}
     starNotFound = True
-    for star in starList:
-        for exoStar in exoStarList:
-            if areEqual(float(star.field("index_ra")),float(exoStar["ra"])) and areEqual(float(star.field("index_dec")), float(exoStar["dec"])):
-                starNotFound = False
-                ooi["index_ra"] = star.field("index_ra")
-                ooi["index_dec"] = star.field("index_dec")
-                ooi["x"] = star.field("field_x")
-                ooi["y"] = star.field("field_y")
-                break;
+    for exoStar in exoStarList:
+        if areEqual(exoStar["ra"], wcsDict["ra_center"], 0.166) and areEqual(exoStar["dec"], wcsDict["dec_center"], 0.166):
+            starNotFound = False
+            ooi = exoStar
+            print("the star is: " + ooi["name"])
 
     if starNotFound:
         print("404 - STAR NOT FOUND: " + bfn)
         continue
-    genImexamInsts(ooi, root + bfn + ".imInst")
-    genCooFile(ooi, starList, root + bfn + ".coo")
+
+    f = pyfits.open(bfn + ".rdls")
+    rdSources = f[1].data
+
+    index = date + ooi["name"]
+    #TODO use sets
+    if index in runs:
+        temp = []
+        print(len(runs[index]))
+        count = 0
+        for i in runs[index]:
+            for j in rdSources:
+                if areEqual(i[0], j[0]) and areEqual(i[1], j[1]):
+                    exists =  False
+                    for k in temp:
+                        if areEqual(i[0], k[0]) and areEqual(k[1], i[1]):
+                            exists = True
+                    if not exists:
+                        print(str(i[0]) + ", " + str(j[0]))
+                        count += 1
+                        temp.append(i)
+        print(count)
+        runs[index] = temp
+    else:
+        runs[index] = rdSources
+
+#By here runs contains all the run info necessary to do photometry
+for index in runs:
+    print(index)
+    print(runs[index])
