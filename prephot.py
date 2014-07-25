5602623138895ed968cd75a30755454babaedbf4
NiBr B <NiBr@MIT.edu>
2014-07-25 02:12:51 -0400

    Moved to use a basefilename instead of .corr fns
    Also changed a few variable names to make more sense. No real
    algorithmic changes.

--- prephot.py
+++ prephot.py
@@ -6,26 +6,27 @@ import pyfits
 
 #get all .corr
 root = os.getcwd()+"/"
-corrFnList = [os.path.relpath(os.path.join(dirpath, f), root)
+baseFnList = [os.path.splitext(os.path.relpath(os.path.join(dirpath, f), root))[0]
                     for dirpath, dirnames, files in os.walk(root)
-                                for f in fnmatch.filter(files, "*.corr")]
+                                for f in fnmatch.filter(files, "*.solved")]
+baseFnList.sort()
 
 def genCooFile(ooi, starList, fn):
-    #We want field_x&y for center
+    #We want x&y for center
     #We want index_ra&dec for coords
     #Would be interesting to see if flux&background give enough to lightcurve...
     coords = []
-    coords.append(str(int(ooi["field_x"])) + " " + str(int(ooi["field_y"])))
+    coords.append(str(int(ooi["x"])) + " " + str(int(ooi["y"])))
     for line in starList:
-        if int(ooi["field_x"]) != int(line.field("field_x")) or int(ooi["field_y"]) != int(line.field("field_y")):
-            coords.append(str(int(line.field("field_x"))) + " " + str(int(line.field("field_y"))))
+        if int(ooi["x"]) != int(line.field("x")) or int(ooi["y"]) != int(line.field("y")):
+            coords.append(str(int(line.field("x"))) + " " + str(int(line.field("y"))))
 
     fs.writeListToFile(fn, coords)
     return coords
 
 def genImexamInsts(star, fn):
     #figure out which is target star
-    insts = [':s ' + str(int(star["field_x"])) + ' ' + str(int(star["field_y"]))]
+    insts = [':s ' + str(int(star["x"])) + ' ' + str(int(star["y"]))]
 
     fs.writeListToFile(fn, insts)
     return insts
@@ -34,34 +35,41 @@ def genImexamInstsAlt(starList, fn):
     #figure out which is target star
     insts = []
     for star in starList:
-        insts.append(':s ' + str(star.field('field_x')) + ' ' + str(star.field('field_y')))
+        insts.append(':s ' + str(star.field('x')) + ' ' + str(star.field('y')))
 
     fs.writeListToFile(fn, insts)
     return insts
 
 def getExoplanetList():
     exoplanetListFn = "/home/astron/bin/atlas/exoplanetList"
-    return [i.split(",") for i in fs.readFileToArray(exoplanetListFn)]
+    exoList = [i.split(",") for i in fs.readFileToArray(exoplanetListFn)]
+    exoDic = []
+    for line in exoList:
+        dic = {}
+        dic["name"] = line[0]
+        dic["ra"] = line[1]
+        dic["dec"] = line[2]
+        exoDic.append(dic)
+    return exoDic
 
 def areEqual(a, b, precision = 3):
     return abs(a-b) <= pow(10, -precision)
 
 exoStarList = getExoplanetList()
-for corrFn in corrFnList:
-    print("opening " + corrFn)
-    f = pyfits.open(corrFn)
-    bfn = os.path.splitext(corrFn)[0]
+for bfn in baseFnList:
+    print("opening " + bfn)
+    f = pyfits.open(bfn + ".new")
     starList = f[1].data
     ooi = {}
     starNotFound = True
     for star in starList:
         for exoStar in exoStarList:
-            if areEqual(float(star.field("index_ra")),float(exoStar[0])) and areEqual(float(star.field("index_dec")), float(exoStar[1])):
+            if areEqual(float(star.field("index_ra")),float(exoStar["ra"])) and areEqual(float(star.field("index_dec")), float(exoStar["dec"])):
                 starNotFound = False
                 ooi["index_ra"] = star.field("index_ra")
                 ooi["index_dec"] = star.field("index_dec")
-                ooi["field_x"] = star.field("field_x")
-                ooi["field_y"] = star.field("field_y")
+                ooi["x"] = star.field("field_x")
+                ooi["y"] = star.field("field_y")
                 break;
 
     if starNotFound:
